69a4814ad811e51cf4c2db78fc812fa1140f983c
Biwott362 <biwottttt@gmail.com>
2024-05-13 23:42:41 +0300

    Put Css and Js in one file
    put body of css and javascript in one file html

--- script.js
+++
@@ -1,8 +0,0 @@
-let form = document.querySelector("#form");
-
-form.addEventListener('submit',function(event){
-    event.preventDefault();
-    let email = document.querySelector("#email").value;
-    alert (`Thanks ${email} we'll be in touch!`);
-
-})
